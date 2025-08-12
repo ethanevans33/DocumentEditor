850b06d76d6e6e05513c5750dc2e4894c457a74c
Ethan Evans <etevans2327@gmail.com>
2025-08-12 00:23:36 -0500

    Added filed needed for correct project structure

---
+++ src/app/documents/page.tsx
@@ -0,0 +1,11 @@
+const DocumnetsPage = () => {
+    return (
+        <div>
+            <p>
+                Documnets Page
+            </p>
+        </div>
+    );
+}
+
+export default DocumnetsPage;

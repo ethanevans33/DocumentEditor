af8eaa6e050490f66283d3b757b3dad4f4eae00d
Ethan Evans <etevans2327@gmail.com>
2025-08-12 00:34:57 -0500

    Added basic tiptap editor with StarterKit

---
+++ src/app/documents/[documentId]/editor.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useEditor, EditorContent } from '@tiptap/react';
+import StarterKit from '@tiptap/starter-kit';
+
+export const Editor = () => {
+    const editor = useEditor({
+        extensions: [StarterKit],
+        content: '<p>Hello World! 🌍</p>',
+    });
+    return (
+        <div>
+            <EditorContent editor={editor} />
+        </div>
+    );
+}

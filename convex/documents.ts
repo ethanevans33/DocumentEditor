26c9b6695ebc16ee4f7f988a328ba656eec441c8
Ethan Evans <etevans2327@gmail.com>
2025-08-14 03:58:40 -0500

    Added search functionality for documnets

--- convex/documents.ts
+++ convex/documents.ts
@@ -1,6 +1,7 @@
 import { ConvexError, v } from "convex/values";
 import { paginationOptsValidator } from "convex/server";
 import { mutation, query } from "./_generated/server";
+import { Underdog } from "next/font/google";
 
 export const create = mutation({
   args: { title: v.optional(v.string()), initialContent: v.optional(v.string()) },
@@ -20,9 +21,27 @@ export const create = mutation({
 });
 
 export const get = query({
-  args: { paginationOpts: paginationOptsValidator },
-  handler: async (ctx, args) => {
-    return await ctx.db.query("documents").paginate(args.paginationOpts);
+  args: { paginationOpts: paginationOptsValidator, search: v.optional(v.string()) },
+  handler: async (ctx, { search, paginationOpts }) => {
+    const user = await ctx.auth.getUserIdentity();
+
+      if(!user){
+        throw new ConvexError("Unauthorized");
+      }
+
+      if (search){
+        return await ctx.db
+          .query("documents")
+          .withSearchIndex("search_title", (q) =>
+            q.search("title", search).eq("ownerId", user.subject)
+        )
+        .paginate(paginationOpts)
+      }
+
+    return await ctx.db
+      .query("documents")
+      .withIndex("by_owner_id", (q) => q.eq("ownerId", user.subject))
+      .paginate(paginationOpts);
   },
 });
 
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,13 +5,15 @@ import { Navbar } from "./navbar";
 import { DocumentsTable } from "./documents-table";
 import { TemplatesGallery } from "./templates.gallery";
 import { api } from "../../../convex/_generated/api";
+import { useSearchParam } from "@/hooks/use-search-param";
 
 const Home = () => {
+  const [search] = useSearchParam();
   const {
     results, 
     status, 
     loadMore
-  } = usePaginatedQuery(api.documents.get, {}, {initialNumItems: 5});
+  } = usePaginatedQuery(api.documents.get, { search }, {initialNumItems: 5});
 
   return (
     <div className="min-h-screen flex flex-col">

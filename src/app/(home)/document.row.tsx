72d29fd028e11d6410d971e1fec04c693e95edad
Ethan Evans <etevans2327@gmail.com>
2025-08-14 03:47:15 -0500

    Added remove and rename components for deleting and renaming documents with confirmation and form handling

--- src/app/(home)/document.row.tsx
+++ src/app/(home)/document.row.tsx
@@ -2,16 +2,20 @@ import { TableCell, TableRow } from "@/components/ui/table";
 import { SiGoogledocs } from "react-icons/si";
 import { format } from "date-fns";
 import { Doc } from "../../../convex/_generated/dataModel";
-import { Building2Icon, CircleUserIcon, MoreVertical } from "lucide-react";
-import { Button } from "@/components/ui/button";
+import { Building2Icon, CircleUserIcon } from "lucide-react";
+import { DocumentMenu } from "./document-menu";
+import { useRouter } from "next/navigation";
 
 interface DocumentRowProps {
     document: Doc<"documents">;
 };
 
 export const DocumentRow = ({ document }: DocumentRowProps) => {
+    const router = useRouter();
+
     return (
         <TableRow
+        onClick={() => router.push('/documents/${documnet._id}')}
         className="cursor-pointer"
         >
             <TableCell className="w-[50px]">
@@ -31,9 +35,12 @@ export const DocumentRow = ({ document }: DocumentRowProps) => {
                 {format(new Date(document._creationTime), "MMM dd, yyyy")}
             </TableCell>
             <TableCell className="flex justify-end">
-                <Button variant="ghost" size="icon" className="rounded-full">
-                    <MoreVertical className="size-4" />
-                </Button>
+                <DocumentMenu 
+                    documentId={document._id}
+                    title={document.title}
+                    onNewTab={() => window.open('/documents/${document._id}', "_blank")}
+                />
+
             </TableCell>
         </TableRow>
     )

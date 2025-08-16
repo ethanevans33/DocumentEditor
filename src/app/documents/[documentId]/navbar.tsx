63af92f500b6a7b84cfa94d77919a7af788e19ac
Ethan Evans <etevans2327@gmail.com>
2025-08-15 23:05:10 -0500

    Added room provider, avatars component, and clerk user fetching for liveblocks collaboration

--- src/app/documents/[documentId]/navbar.tsx
+++ src/app/documents/[documentId]/navbar.tsx
@@ -35,6 +35,7 @@ import {
     MenubarTrigger,
 } from "@/components/ui/menubar"
 
+import { Avatars } from "./avatars";
 import { DocumentInput } from "./document-input";
 import { useEditorStore } from "@/store/use-editor-store";
 import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
@@ -226,6 +227,7 @@ export const Navbar = () => {
                 </div>
             </div>
             <div className="flex gap-3 items-center pl-6">
+                <Avatars />
                 <OrganizationSwitcher 
                 afterCreateOrganizationUrl="/"
                 afterLeaveOrganizationUrl="/"
